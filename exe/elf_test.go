3797775ff78039bc91510225e781cf6620ce7765
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2025-03-04 23:53:42 -0600

    NewElf test written and passing

--- exe/elf_test.go
+++ exe/elf_test.go
@@ -1,9 +1,76 @@
 package exe
 
 import (
+	"encoding/hex"
+	"fmt"
+	"iago/isa"
+	"os"
 	"testing"
 )
 
-func FooTest(t *testing.T) {
-	t.Log("What does this do?")
+var testBinaries = map[string][]byte {}
+
+// reads in test binaries
+func setup() {
+	testPath := "../test_programs/bin/"
+	dirEntries, err := os.ReadDir(testPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if len(dirEntries) == 0 {
+		fmt.Println("Compile the test programs before running tests")
+		os.Exit(1)
+	}
+	for _, f  := range dirEntries {
+		name := f.Name()
+		fmt.Println(name)
+		fileContents, err := os.ReadFile(testPath + name)
+		if err != nil {
+			fmt.Println("Error reading test binary")
+			os.Exit(1)
+		}
+		testBinaries[name] = fileContents
+	}
+	// added in error case
+	corruptFile, err := hex.DecodeString("df118e9f9cc24bd9a1c989d57fc28976568d1f49b2e19352f81d48ef1b41f2595e1b6ec3e7553deb680d76b3aeb7e7faa576baec526553be4bc5c1c5900c2450851151ddef4031d69a30843750753215ec22a811fe02e73fee80df7db60d0bff6d80f43ac3d2116fc230f59e8d463f66f7442bc85f2a717f92b4c6ab6db347e3")
+	if err != nil {
+		fmt.Println("Error decoding hex string")
+		os.Exit(1)
+	}
+	testBinaries["corrupt"] = corruptFile
+}
+
+func TestMain(m *testing.M) {
+	setup()
+}
+
+func TestNewElf(t *testing.T) {
+	var expectedResults = map[string]struct {
+		Arch uint
+		End string
+		Isa isa.ISA
+		} {
+		"square32": {Arch: 32, End: "little", Isa: isa.X86{}},
+		"square64": {Arch: 64, End: "little", Isa: isa.X86{}},
+	}
+	for name, contents := range testBinaries {
+		expected := expectedResults[name]
+		actual, err := NewElf(contents)
+		if name == "corrupt" {
+			if err == nil {
+				t.Fail()
+			}
+		} else {
+			if actual.(*Elf).arch != expected.Arch {
+				t.Fail()
+			}
+			if actual.(*Elf).endianness != expected.End {
+				t.Fail()
+			}
+			if actual.(*Elf).isa != expected.Isa {
+				t.Fail()
+			}
+		}
+	}
 }
