02f0e3454111b5d24d366e2829692c7628e7e247
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2025-03-26 18:49:56 -0500

    added to help command

--- src/cli/help.go
+++ src/cli/help.go
@@ -2,9 +2,11 @@ package cli
 
 import (
 	"fmt"
-	"strings"
 	"iago/src/global"
+	"strings"
 )
+
+const SPACE_BETWEEN = 24
 
 type Help struct{ args Args }
 
@@ -14,11 +16,13 @@ func (h Help) ValidArgs() bool {
 
 func (Help) Execute(_ *global.GlobalState) error {
 	fmt.Println("Commands:")
-	fmt.Println("    exit" + strings.Repeat(" ", 16-len("quit")) + "Exit the interactive shell")
-	fmt.Println("    help" + strings.Repeat(" ", 16-len("help")) + "Show help")
-	fmt.Println("    load <path>" + strings.Repeat(" ", 16-len("load <path>")) + "Sets the current file for analysis")
-	fmt.Println("    stat" + strings.Repeat(" ", 16-len("stat")) + "View the current file's metadata")
-	fmt.Println("    set-target" + strings.Repeat(" ", 16-len("set-target")) + "")
-	fmt.Println("    target" + strings.Repeat(" ", 16-len("target")) + "View the current target payload")
+	fmt.Println("    exit" + strings.Repeat(" ", SPACE_BETWEEN-len("quit")) + "Exit the interactive shell")
+	fmt.Println("    help" + strings.Repeat(" ", SPACE_BETWEEN-len("help")) + "Show help")
+	fmt.Println("    load <path>" + strings.Repeat(" ", SPACE_BETWEEN-len("load <path>")) + "Sets the current file for analysis")
+	fmt.Println("    rop [OPTIONS]" + strings.Repeat(" ", SPACE_BETWEEN-len("rop [OPTIONS]")) + "Generate a ROP chain for the target file and payload")
+	fmt.Println("        -o FILE  Specify output file (optional)")
+	fmt.Println("    stat" + strings.Repeat(" ", SPACE_BETWEEN-len("stat")) + "View the current file's metadata")
+	fmt.Println("    cat-target" + strings.Repeat(" ", SPACE_BETWEEN-len("cat-target")) + "View the current target payload")
+	fmt.Println("    set-target <path>" + strings.Repeat(" ", SPACE_BETWEEN-len("set-target <path>")) + "Set the target payload")
 	return nil
 }
