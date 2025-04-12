740785f9cb8c2be7a328106af04208275ce4877a
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2025-04-11 21:59:31 -0500

    reordered help menu

--- src/cli/help.go
+++ src/cli/help.go
@@ -16,15 +16,16 @@ func (h Help) ValidArgs() bool {
 
 func (Help) Execute(_ *global.GlobalState) error {
 	fmt.Println("Commands:")
-	fmt.Println("    exit" + strings.Repeat(" ", SPACE_BETWEEN-len("quit")) + "Exit the interactive shell")
-	fmt.Println("    help" + strings.Repeat(" ", SPACE_BETWEEN-len("help")) + "Show help")
+	fmt.Println("    help" + strings.Repeat(" ", SPACE_BETWEEN-len("help")) + "View this help menu")
 	fmt.Println("    load <path>" + strings.Repeat(" ", SPACE_BETWEEN-len("load <path>")) + "Sets the current file for analysis")
-	fmt.Println("    rop [OPTIONS]" + strings.Repeat(" ", SPACE_BETWEEN-len("rop [OPTIONS]")) + "Generate a ROP chain for the target file and payload")
-	fmt.Println("        -o FILE  Specify output file (optional)")
 	fmt.Println("    stat" + strings.Repeat(" ", SPACE_BETWEEN-len("stat")) + "View the current file's metadata")
 	fmt.Println("    cat-target" + strings.Repeat(" ", SPACE_BETWEEN-len("cat-target")) + "View the current target payload")
 	fmt.Println("    set-target <path>" + strings.Repeat(" ", SPACE_BETWEEN-len("set-target <path>")) + "Set the target payload")
 	fmt.Println("    cat-payload" + strings.Repeat(" ", SPACE_BETWEEN-len("cat-payload")) + "View the current payload")
 	fmt.Println("    set-payload <path>" + strings.Repeat(" ", SPACE_BETWEEN-len("set-payload <path>")) + "Set the current payload")
+	fmt.Println("    rop [OPTIONS]" + strings.Repeat(" ", SPACE_BETWEEN-len("rop [OPTIONS]")) + "Generate a ROP chain for the target file and payload")
+	fmt.Println("        -o FILE  Specify output file (optional)")
+	fmt.Println("    quit" + strings.Repeat(" ", SPACE_BETWEEN-len("quit")) + "Exit the interactive shell")
+
 	return nil
 }
