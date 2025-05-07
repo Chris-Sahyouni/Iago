feeec53374ee6c8a47b7ccda3cf04ce69410641c
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2025-05-07 16:59:08 -0500

    moved main.go to root

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"iago/src/cli"
+	"iago/src/global"
+	"iago/src/term"
+)
+
+func main() {
+
+	fmt.Println("Iago interactive shell")
+	fmt.Println("Run help to view available commands")
+
+	terminal := term.RawTerminal()
+
+	globalState := global.GlobalState{
+		CurrentFile: nil,
+		CurrentTarget: struct {
+			Title    string
+			Contents string
+		}{
+			Title:    "",
+			Contents: "",
+		},
+		CurrentPayload: struct {
+			PaddingLength int
+			Chain         []uint
+		}{
+			PaddingLength: 0,
+			Chain:         nil,
+		},
+		Terminal: terminal,
+	}
+
+
+	for {
+		line, err := terminal.ReadLine()
+		cmd, err := cli.ParseLine(line)
+		if err != nil {
+			term.Println(err)
+			continue
+		}
+		if !cmd.ValidArgs() {
+			term.Println("invalid arguments")
+		}
+		err = cmd.Execute(&globalState)
+		if err != nil {
+			term.Println(err)
+		}
+
+	}
+
+}
